22e93c23fa1aca9ba8c7017cc5a34b800114d002
Welle77 <simon.wessmann.brok@gmail.com>
2021-12-29 12:20:29 +0100

    seems nice, copy most

--- src/components/MainStoryGrid/MainStoryGrid.js
+++ src/components/MainStoryGrid/MainStoryGrid.js
@@ -1,79 +1,144 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from 'react'
+import styled from 'styled-components/macro'
 
-import {
-  MAIN_STORY,
-  OPINION_STORIES,
-  SECONDARY_STORIES,
-} from '../../data';
+import { MAIN_STORY, OPINION_STORIES, SECONDARY_STORIES } from '../../data'
 
-import SectionTitle from '../SectionTitle';
-import MainStory from '../MainStory';
-import SecondaryStory from '../SecondaryStory';
-import OpinionStory from '../OpinionStory';
-import Advertisement from '../Advertisement';
+import SectionTitle from '../SectionTitle'
+import MainStory from '../MainStory'
+import SecondaryStory from '../SecondaryStory'
+import OpinionStory from '../OpinionStory'
+import Advertisement from '../Advertisement'
+import { QUERIES } from '../../constants'
 
 const MainStoryGrid = () => {
-  return (
-    <Wrapper>
-      <MainStorySection>
-        <MainStory {...MAIN_STORY} />
-      </MainStorySection>
+    return (
+        <Wrapper>
+            <MainStorySection>
+                <MainStory {...MAIN_STORY} />
+            </MainStorySection>
 
-      <SecondaryStorySection>
-        <StoryList>
-          {SECONDARY_STORIES.map((story, index) => (
-            <SecondaryStory key={story.id} {...story} />
-          ))}
-        </StoryList>
-      </SecondaryStorySection>
+            <SecondaryStorySection>
+                <StoryList>
+                    {SECONDARY_STORIES.map((story, index) => (
+                        <VeticalStoryWrapper key={story.id}>
+                            <SecondaryStory {...story} />
+                        </VeticalStoryWrapper>
+                    ))}
+                </StoryList>
+            </SecondaryStorySection>
 
-      <OpinionSection>
-        <SectionTitle>Opinion</SectionTitle>
-        <StoryList>
-          {OPINION_STORIES.map((story, index) => (
-            <OpinionStory key={story.id} {...story} />
-          ))}
-        </StoryList>
-      </OpinionSection>
+            <OpinionSection>
+                <SectionTitle>Opinion</SectionTitle>
+                <OpinionStoryList>
+                    {OPINION_STORIES.map((story, index) => (
+                        <OpinionStoryWrapper key={story.id}>
+                            <OpinionStory {...story} />
+                        </OpinionStoryWrapper>
+                    ))}
+                </OpinionStoryList>
+            </OpinionSection>
 
-      <AdvertisementSection>
-        <Advertisement />
-      </AdvertisementSection>
-    </Wrapper>
-  );
-};
+            <AdvertisementSection>
+                <Advertisement />
+            </AdvertisementSection>
+        </Wrapper>
+    )
+}
 
 const Wrapper = styled.div`
-  display: grid;
-  grid-template-areas:
-    'main-story'
-    'secondary-stories'
-    'opinion-stories'
-    'advertisement';
-  gap: 48px;
-  margin-bottom: 48px;
-`;
+    display: grid;
+    grid-template-areas:
+        'main-story'
+        'secondary-stories'
+        'opinion-stories'
+        'advertisement';
+    gap: 48px;
+    margin-bottom: 48px;
+
+    @media ${QUERIES.tabletAndUp} {
+        grid-template-areas:
+            'main-story secondary-stories'
+            'advertisement advertisement'
+            'opinion-stories opinion-stories';
+        grid-template-columns: 2fr 1fr;
+        gap: 48px 0px;
+    }
+
+    @media ${QUERIES.laptopAndUp} {
+        grid-template-areas:
+            'main-story secondary-stories opinion-stories'
+            'main-story advertisement advertisement';
+        grid-template-columns: 5fr 4fr 3fr;
+        gap: 0px;
+    }
+`
 
 const MainStorySection = styled.section`
-  grid-area: main-story;
-`;
+    grid-area: main-story;
+
+    @media ${QUERIES.tabletAndUp} {
+        padding-right: 16px;
+        margin-right: 16px;
+        border-right: 1px solid var(--color-gray-300);
+    }
+`
+
+const VeticalStoryWrapper = styled.div`
+    &:not(:last-of-type) {
+        border-bottom: 1px solid var(--color-gray-300);
+        padding-bottom: 16px;
+        margin-bottom: 16px;
+    }
+`
 
 const SecondaryStorySection = styled.section`
-  grid-area: secondary-stories;
-`;
+    grid-area: secondary-stories;
+    @media ${QUERIES.laptopAndUp} {
+        padding-right: 16px;
+        margin-right: 16px;
+        border-right: 1px solid var(--color-gray-300);
+    }
+`
 
 const StoryList = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
+    display: flex;
+    flex-direction: column;
+`
 
 const OpinionSection = styled.section`
-  grid-area: opinion-stories;
-`;
+    grid-area: opinion-stories;
+
+    @media ${QUERIES.laptopAndUp} {
+        margin-top: -8px;
+    }
+`
+const OpinionStoryList = styled(StoryList)`
+    @media ${QUERIES.tabletOnly} {
+        flex-direction: row;
+        gap: 32px;
+    }
+`
+
+const OpinionStoryWrapper = styled(VeticalStoryWrapper)`
+    @media ${QUERIES.tabletOnly} {
+        flex: 1;
+
+        &:not(:last-of-type) {
+            border-bottom: revert;
+            padding-bottom: revert;
+            margin-bottom: revert;
+        }
+    }
+`
 
 const AdvertisementSection = styled.section`
-  grid-area: advertisement;
-`;
+    grid-area: advertisement;
 
-export default MainStoryGrid;
+    @media ${QUERIES.laptopAndUp} {
+        padding-top: 16px;
+        margin-top: 16px;
+        border-top: 1px solid var(--color-gray-300);
+    }
+`
+
+export default MainStoryGrid
--- src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.js
+++ src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.js
@@ -1,60 +1,99 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from 'react'
+import styled from 'styled-components/macro'
 
-import { MARKET_DATA, SPORTS_STORIES } from '../../data';
+import { MARKET_DATA, SPORTS_STORIES } from '../../data'
 
-import MarketCard from '../MarketCard';
-import SectionTitle from '../SectionTitle';
-import MiniStory from '../MiniStory';
+import MarketCard from '../MarketCard'
+import SectionTitle from '../SectionTitle'
+import MiniStory from '../MiniStory'
+import { QUERIES } from '../../constants'
 
 const SpecialtyStoryGrid = () => {
-  return (
-    <Wrapper>
-      <MarketsSection>
-        <SectionTitle
-          cornerLink={{
-            href: '/markets',
-            content: 'Visit Markets data »',
-          }}
-        >
-          Markets
-        </SectionTitle>
-        <MarketCards>
-          {MARKET_DATA.map((data) => (
-            <MarketCard key={data.tickerSymbol} {...data} />
-          ))}
-        </MarketCards>
-      </MarketsSection>
-      <SportsSection>
-        <SectionTitle
-          cornerLink={{
-            href: '/sports',
-            content: 'Visit Sports page »',
-          }}
-        >
-          Sports
-        </SectionTitle>
-        <SportsStories>
-          {SPORTS_STORIES.map((data) => (
-            <MiniStory key={data.id} {...data} />
-          ))}
-        </SportsStories>
-      </SportsSection>
-    </Wrapper>
-  );
-};
+    return (
+        <Wrapper>
+            <MarketsSection>
+                <SectionTitle
+                    cornerLink={{
+                        href: '/markets',
+                        content: 'Visit Markets data »',
+                    }}
+                >
+                    Markets
+                </SectionTitle>
+                <MarketCards>
+                    {MARKET_DATA.map((data) => (
+                        <MarketCard key={data.tickerSymbol} {...data} />
+                    ))}
+                </MarketCards>
+            </MarketsSection>
+            <SportsSection>
+                <SectionTitle
+                    cornerLink={{
+                        href: '/sports',
+                        content: 'Visit Sports page »',
+                    }}
+                >
+                    Sports
+                </SectionTitle>
+                <SportsStories>
+                    {SPORTS_STORIES.map((data) => (
+                        <SportStoryWrapper key={data.id}>
+                            <MiniStory {...data} />
+                        </SportStoryWrapper>
+                    ))}
+                </SportsStories>
+            </SportsSection>
+        </Wrapper>
+    )
+}
 
 const Wrapper = styled.div`
-  display: grid;
-  gap: 48px;
-`;
+    display: grid;
+    gap: 48px;
 
-const MarketsSection = styled.section``;
+    @media ${QUERIES.tabletAndUp} {
+        gap: 48px;
+        grid-template-columns: minmax(0px, auto);
+    }
 
-const MarketCards = styled.div``;
+    @media ${QUERIES.laptopAndUp} {
+        gap: 48px 0px;
+        grid-template-columns: 1fr minmax(0px, 1fr);
+    }
+`
 
-const SportsSection = styled.section``;
+const MarketsSection = styled.section`
+    @media ${QUERIES.laptopAndUp} {
+        padding-right: 16px;
+        margin-right: 16px;
+        border-right: 1px solid var(--color-gray-300);
+    }
+`
 
-const SportsStories = styled.div``;
+const MarketCards = styled.div`
+    display: grid;
+    gap: 16px;
+    grid-template-columns: repeat(auto-fill, minmax(min(165px, 100%), 1fr));
+`
 
-export default SpecialtyStoryGrid;
+const SportsSection = styled.section``
+
+const SportsStories = styled.div`
+    display: grid;
+    gap: 16px;
+    grid-template-columns: repeat(auto-fill, minmax(min(165px, 100%), 1fr));
+
+    @media ${QUERIES.tabletAndUp} {
+        display: flex;
+        grid-template-columns: revert;
+        overflow: auto;
+    }
+`
+
+const SportStoryWrapper = styled.div`
+    @media ${QUERIES.tabletAndUp} {
+        min-width: 220px;
+    }
+`
+
+export default SpecialtyStoryGrid
